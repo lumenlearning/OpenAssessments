a7edbbc61009f4c480d6825f0c572263f69eb38c
Justin Ball <justinball@gmail.com>
2015-03-11 18:51:43 -0600

    Added canvas login form.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def canvas_url
+    session[:canvas_url] || Rails.application.secrets.canvas_url
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,15 @@ Rails.application.routes.draw do
 
   root :to => "home#index"
 
-  devise_for :users, :controllers => {
-    :registrations => "registrations",
-    :omniauth_callbacks => "omniauth_callbacks"
+  devise_for :users, controllers: {
+    sessions: "sessions",
+    registrations: "registrations",
+    omniauth_callbacks: "omniauth_callbacks"
   }
+  
   resources :users
-
-  match '/evaluations', to: 'evaluations#index', via: [:get, :post]
-
+  resources :canvas_authentications
+  
   mount MailPreview => 'mail_view' if Rails.env.development?
 
 end
