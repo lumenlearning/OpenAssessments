5aad8d30544fae9a7192b8865d89311d436258a9
Justin Ball <justinball@gmail.com>
2015-03-16 15:26:03 -0600

    Don't delete authentications on logout.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < Devise::SessionsController
   
   def destroy
-    current_user.authentications.where(provider: 'canvas').destroy_all
+    #current_user.authentications.where(provider: 'canvas').destroy_all
     super
   end
 
