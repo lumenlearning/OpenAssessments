9378be2cea51ee3fffc42e974a0c1be515fa7018
Justin Ball <justinball@gmail.com>
2015-03-19 09:21:27 -0600

    Added example to home controller.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
   
   def index
+    auth = current_user.authentications.find_by(provider: 'canvas')
+    api = Canvas.new(current_account.canvas_uri, auth.token)
   end
 
 end
