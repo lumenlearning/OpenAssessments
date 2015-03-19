92b2ef1646c9f0a8011b1b477d30591f48dd1832
Justin Ball <justinball@gmail.com>
2015-03-19 09:19:03 -0600

    Switch to CanCanCan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  include Pundit
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -9,8 +9,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
-    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
+    rescue_from CanCan::AccessDenied do |exception|
+      redirect_to root_url, :alert => exception.message
+    end
+    
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) << :name
       devise_parameter_sanitizer.for(:account_update) << :name
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,16 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
+  load_and_authorize_resource
+
   after_action :verify_authorized, except: [:show]
 
   def index
-    @users = User.all
-    authorize @users
   end
 
   def show
-    @user = User.find(params[:id])
-    unless current_user.admin?
-      unless @user == current_user
-        redirect_to :back, :alert => "Access denied."
-      end
-    end
   end
 
   def update
-    @user = User.find(params[:id])
-    authorize @user
     if @user.update_attributes(secure_params)
       redirect_to users_path, :notice => "User updated."
     else
@@ -27,20 +19,18 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    user = User.find(params[:id])
-    authorize user
-    unless user == current_user
-      user.destroy
-      redirect_to users_path, :notice => "User deleted."
-    else
-      redirect_to users_path, :notice => "Can't delete yourself."
-    end
+    user.destroy
+    redirect_to users_path, :notice => "User deleted."
   end
 
   private
 
-  def secure_params
-    params.require(:user).permit(:role)
-  end
+    def create_params
+      params.require(:user).permit(:role)
+    end
+
+    def update_params
+      params.require(:user).permit(:role)
+    end
 
 end
