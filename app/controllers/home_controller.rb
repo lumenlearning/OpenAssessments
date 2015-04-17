31fe769b69f42a819129e115e0ec37e69c3110e6
Justin Ball <justinball@gmail.com>
2015-04-17 17:39:28 -0600

    Remove auth requirement from home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
-  before_filter :authenticate_user!
+  
+  #before_filter :authenticate_user!
   
   def index
-    auth = current_user.authentications.find_by(provider: 'canvas')
-    api = Canvas.new(current_account.canvas_uri, auth.token)
+    #auth = current_user.authentications.find_by(provider: 'canvas')
+    #api = Canvas.new(current_account.canvas_uri, auth.token)
   end
 
 end
