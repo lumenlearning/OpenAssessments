ccfb919db3d02a352fa845b3dc5e24566130c94b
dittonjs <jditton.atomic@gmail.com>
2015-04-20 15:46:20 -0600

    Integrated webpac with rails

--- client/webpack.hot.js
+++ client/webpack.hot.js
@@ -2,19 +2,25 @@ var webpack           = require('webpack');
 var webpackDevServer  = require('webpack-dev-server');
 var release           = false;
 var settings          = require('./config/settings.js');
-var webpackConfig     = require('./config/webpack.config.js')(release);
+var webpackConfig     = require('./config/webpack.config.js');
+
 
 new webpackDevServer(webpack(webpackConfig), {
   publicPath: webpackConfig.output.publicPath,
   hot: true,
+  watch: true,
   noInfo: true,
   colors: true,
   inline: true,
   progress: true,
-  headers: { "Access-Control-Allow-Origin": "*" }
+  headers: { "Access-Control-Allow-Origin": "*" },
+  filename: "app_wp_bundle.js",
+  noInfo: false,
+  quiet: false,
 }).listen(settings.ports.hotPort, 'localhost', function(err, result){
   if(err){
     console.log('webpack-dev-server', err);
   }
+  console.log(webpackConfig.output.filename);
   console.log('Webpack hot load server listening on: ' + webpackConfig.output.publicPath);
 });
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( evaluations.js )
+Rails.application.config.assets.precompile += %w( app_wp_bundle.js )
