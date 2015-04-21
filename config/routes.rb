1b3e57dccd02a887b79f47c988a7c86050392bce
Justin Ball <justinball@gmail.com>
2015-04-21 11:38:55 -0600

    Added OEA routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,33 @@ Rails.application.routes.draw do
     omniauth_callbacks: "omniauth_callbacks"
   }
   
-  resources :users
+  resources :users do
+    resources :assessments, except: [:update, :edit], :controller => "assessments"
+  end
   resources :canvas_authentications
+  
+  resources :assessments
+  resources :assessment_loaders
+  resources :assessment_results
+  resources :item_results
+
+  get 'saml', to: 'saml#index'
+  get 'saml/metadata', to: 'saml#metadata'
+  post 'saml/consume', to: 'saml#consume'
+
+  # oembed
+  match 'oembed' => 'oembed#endpoint', :via => [:get, :post]
+
+  namespace :api do
+    resources :assessments
+    resources :assessment_results
+    resources :item_results
+  end
+
+  match '/proxy' => 'default#proxy', via: [:get, :post]
+  match '/contact' => 'default#contact', via: [:get, :post]
+  match '/about' => 'default#about', via: [:get]
+  match '/take' => 'default#take', via: [:get]
   
   mount MailPreview => 'mail_view' if Rails.env.development?
 
