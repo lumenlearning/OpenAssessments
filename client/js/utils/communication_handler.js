879b430af559b9553526956c3bb0518e3fdd852a
Justin Ball <justinball@gmail.com>
2015-04-27 08:07:07 -0600

    Added utility libraries.

---
+++ client/js/utils/communication_handler.js
@@ -0,0 +1,26 @@
+import $ from 'jquery';
+import Communicator from '../utils/communicator';
+
+export default {
+
+  init: function(){
+    Communicator.enableListener(this);
+  },
+
+  sendSize: function(){
+    var payload = {
+      height: $(document).height(),
+      width: $(document).width()
+    };
+    Communicator.commMsg('open_assessments_resize', payload);
+  },
+
+  handleComm: function(e){
+    switch(e.data.open_assessments_msg){
+      case 'open_assessments_size_request':
+        this.sendSize();
+        break;
+    }
+  }
+
+};
