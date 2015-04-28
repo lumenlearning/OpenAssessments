e1e36b4c94821cef725320833055ec6b1133191f
Justin Ball <justinball@gmail.com>
2015-04-28 12:20:02 -0600

    Added note

--- client/webpack.tests.js
+++ client/webpack.tests.js
@@ -1,6 +1,7 @@
 // require all modules ending in ".spec.js" from the
 // js directory and all subdirectories
 
+
 // Create a Webpack require context so we can dynamically require our
 // project's modules. Exclude test files in this context.
 var context = require.context("./js", true, /\.spec\.js$/);
@@ -9,6 +10,7 @@ var context = require.context("./js", true, /\.spec\.js$/);
 var projectModuleIds = context.keys().map(module =>
   String(context.resolve(module)));
 
+// See http://kentor.me/posts/testing-react-and-flux-applications-with-karma-and-webpack/
 beforeEach(() => {
   // Remove our modules from the require cache before each test case.
   projectModuleIds.forEach(id => delete require.cache[id]);
