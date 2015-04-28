b17a79c1888a216a3baf492b329c4e53a9fe4d9f
Justin Ball <justinball@gmail.com>
2015-04-28 10:16:23 -0600

    Setup settings in layout

--- client/js/components/index.jsx
+++ client/js/components/index.jsx
@@ -7,12 +7,6 @@ import {RouteHandler}       from "react-router";
 export default React.createClass({
 
   render(){
-
-    var style = Settings.style();
-    if(style){
-      $('head').append('<link href="' + style + '" media="all" rel="stylesheet">');
-    }
-
     return (
       <RouteHandler {...this.props} />
     );
