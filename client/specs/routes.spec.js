a4e9a47522e92abbba70204354d7aab46b179704
Justin Ball <justinball@gmail.com>
2015-04-28 10:12:18 -0600

    Updated default route spec

--- client/specs/routes.spec.js
+++ client/specs/routes.spec.js
@@ -1,14 +1,15 @@
 import React    from 'react';
 import Router   from 'react-router';
 
-import routes   from '../routes';
+import routes   from '../js/routes';
+import Settings from '../js/utils/settings';
 
 let Route = Router.Route;
 
 describe('default route', function () {
-  it('renders home', function (done) {
+  it('renders assessment', function (done) {
     Router.run(routes, '/', function (Handler, state){
-      var html = React.renderToString(<Handler/>);
+      var html = React.renderToString(<Handler params={state.params} settings={Settings.load()} />);
       expect(html).toMatch(/Home/);
       done();
     });
