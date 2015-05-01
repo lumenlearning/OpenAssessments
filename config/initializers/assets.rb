40d2033aebe24122cdf64c297597dd1c3e97e29c
Justin Ball <justinball@gmail.com>
2015-05-01 14:54:15 -0600

    Added missing assets back in to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,5 +7,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( evaluations.js app_admin_wp_bundle.js app_wp_bundle.js styles_wp_bundle.js)
-Rails.application.config.assets.precompile += %w( app_admin_wp_bundle.css )
+Rails.application.config.assets.precompile += %w( html5shiv.js open_comm.js evaluations.js app_admin_wp_bundle.js app_wp_bundle.js styles_wp_bundle.js)
+Rails.application.config.assets.precompile += %w( app_admin_wp_bundle.css retina.png bare.css themes/* )
