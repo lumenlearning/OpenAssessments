04fd50a52e4fa61fcb45c796bcfeb19dc8eecaf4
KamiJean <kamijeanwilson@gmail.com>
2015-05-01 13:50:21 -0600

    Adding tests to the qti multiple choice component

---
+++ client/js/components/assessments/qti_multiple_choice.spec.js
@@ -0,0 +1,29 @@
+import React              from 'react';
+import TestUtils          from 'react/lib/ReactTestUtils';
+import QTIMultipleChoice  from './qti_multiple_choice';
+
+describe('qti multiple choice', function() {
+  
+  var items = [
+  	{	
+  		id: 1,
+      name: "QTI Multiple Choice",
+    	material: "The multiple choice label"
+  	},
+  	{	
+  		id: 2,
+      name: "QTI Multiple Choice 2",
+    	material: "The multiple choice label 2"
+  	}
+  ];
+  var result = TestUtils.renderIntoDocument(<QTIMultipleChoice items={items} />);
+  
+  it('renders the radio button label', function() {
+    expect(result.getDOMNode().textContent).toContain(items[0].material);
+  });
+  
+  it('renders input attributes', function() {
+    expect(result.getDOMNode().childNodes[0].childNodes[0].childNodes[0].attributes.name.value).toContain(items[0].name);
+  });
+
+});
