9c622e4e69df778b7edc3a8dd6f320723c951c4f
Justin Ball <justinball@gmail.com>
2015-05-07 22:11:49 -0600

    Handle LTI install for multiple Canvas instances.

--- app/controllers/lti_installs_controller.rb
+++ app/controllers/lti_installs_controller.rb
@@ -2,39 +2,15 @@ class LtiInstallsController < ApplicationController
 
   before_filter :authenticate_user!
   before_filter :validate_account!
+  before_filter :setup, only: [:new, :create]
 
   def new
-
-    auth = current_user.authentications.find_by(provider: 'canvas')
-    if auth
-      api = Canvas.new(current_account.canvas_uri, auth.token)
-      @accounts = api.all_accounts.map{|a| OpenStruct.new(a) }
-      @courses = api.courses.map{|a| OpenStruct.new(a) }
-    else
-      flash[:info] = "Please authenticate with Canvas before attempting to install an LTI tool"
-      redirect_to new_canvas_authentication_path
-    end
-
   end
 
   def create
 
-    auth = current_user.authentications.find_by(provider: 'canvas')
-
-    if auth.blank?
-      flash[:info] = "Please authenticate with Canvas before attempting to install an LTI tool"
-      redirect_to new_canvas_authentication_path
-      return
-    end
-      
-    api = Canvas.new(current_account.canvas_uri, auth.token)
-    
     main_account = current_user.account
 
-    @errors = []
-    @accounts = []
-    @courses = []
-    
     account_code = "#{main_account.code}"
     
     lti_options = {
@@ -52,47 +28,67 @@ class LtiInstallsController < ApplicationController
       } 
     }
     
-    # Install the LTI tool into each selected account
-    api.all_accounts.find_all{|a| params[:lti_install][:account_ids].include?(a['id'].to_s) }.each do |canvas_account|
-      result = Integrations::CanvasAccountsLti.setup(
-        canvas_account, 
-        main_account.lti_key, 
-        main_account.lti_secret,
-        main_account.canvas_uri,
-        auth.token, 
-        lti_options
-      )
-      
-      # Check the result to make sure there wasn't an error
-      if(!result['id'])
-        @errors << canvas_account
-      else
-        @accounts << canvas_account
-      end
+    account_errors, @account_installs = setup_lti(main_account, lti_options, Integrations::CanvasAccountsLti, @accounts, :account_ids)
+    course_errors, @course_installs = setup_lti(main_account, lti_options, Integrations::CanvasCoursesLti, @courses, :course_ids)
 
-    end
+    @errors = account_errors.concat(course_errors)
 
-    # Install the LTI tool into each selected course
-    api.courses.find_all{|a| params[:lti_install][:course_ids].include?(a['id'].to_s) }.each do |canvas_course|
-      result = Integrations::CanvasCoursesLti.setup(
-        canvas_course,
-        main_account.lti_key, 
-        main_account.lti_secret,
-        auth, 
-        lti_options
-      )
-
-      # Check the result to make sure there wasn't an error
-      if(!result['id'])
-        @errors << canvas_course
-      else
-        @courses << canvas_course
-      end
-
-    end
   end
 
   private 
+
+    # This method finds the accounts or courses that were selected by the user, generates
+    # the LTI xml to setup the tool for that account or course and calls the 'setup' method
+    # of either Integrations::CanvasAccountsLti or Integrations::CanvasCoursesLti to
+    # install the LTI tool into the account or course.
+    #
+    # account       - the install will use this accounts lti_key and lti_secrety to setup the tool
+    # lti_options   - the LTI options to pass to the setup method of the lti_interface
+    # lti_interface - either Integrations::CanvasAccountsLti or Integrations::CanvasCoursesLti
+    # collections   - either @accounts or @courses that were built by the 'setup' method
+    # field         - either :account_ids or :course_ids
+    #
+    def setup_lti(account, lti_options, lti_interface, collections, field)
+      errors = []
+      installs = []
+      collections.each do |auth, collection|
+        # Install the LTI tool by calling setup on the lti_interface 
+        collection.find_all{|a| params[:lti_install][field].include?(a['id'].to_s) }.each do |obj|
+          result = lti_interface.method("setup").call(
+            obj, 
+            account.lti_key, 
+            account.lti_secret,
+            auth.provider_url,
+            auth.token, 
+            lti_options
+          )
+          # Check the result to make sure there wasn't an error
+          if(!result['id'])
+            errors << obj
+          else
+            installs << obj
+          end
+        end
+      end
+      [errors, installs]
+    end
+
+    def setup
+      authentications = current_user.authentications.where(provider: 'canvas')
+      @accounts = {}
+      @courses = {}
+
+      if authentications.present?
+        authentications.each do |auth|
+          api = Canvas.new(auth.provider_url, auth.token)
+          @accounts[auth] = api.all_accounts.map{|a| OpenStruct.new(a.merge(auth: auth)) }
+          @courses[auth] = api.courses.map{|a| OpenStruct.new(a.merge(auth: auth)) }
+        end
+      else
+        flash[:info] = "Please authenticate with Canvas before attempting to install an LTI tool"
+        redirect_to new_canvas_authentication_path
+      end
+    end
 
     def validate_account!
       if !current_user.account.present?
