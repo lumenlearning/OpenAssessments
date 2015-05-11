db6cda3359d58f483c97f0fed4e0dd6704bae5ec
Justin Ball <justinball@gmail.com>
2015-05-11 15:09:38 -0600

    Fixed spelling error.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ puts 'CREATED ADMIN USER: ' << user.email
 if Rails.env.production?
   accounts = [{
     code: 'canvasstarterapp',
-    name: 'Canvas Stater App',
+    name: 'Canvas Starter App',
     domain: 'http://www.canvasstarterapp.com',
     lti_key: 'canvasstarterapp',
     lti_secret: '81bb01a80454b7b5a587cfbf1e745d501c0f3b2e3e36b0e60240a2fe7ba8daac0b588678ca8dfaf0d4341017e6646d03751df63c4056a85e01a22e3ce72a8e29',
@@ -14,7 +14,7 @@ if Rails.env.production?
 else
   accounts = [{
     code: 'canvasstarterapp',
-    name: 'Canvas Stater App',
+    name: 'Canvas Starter App',
     domain: 'http://canvasstarterapp.ngrok.com',
     lti_key: 'canvasstarterapp',
     lti_secret: 'd52ca2c9892975bbb9def56e68eefe8e92a338d9b74d73ec5dad64803a376b2f1f5129c0bd9f7e73684526c234e0835bd1635e09d427cd45cb0de4296278682f',
