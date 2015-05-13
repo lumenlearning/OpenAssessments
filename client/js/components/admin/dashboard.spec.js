da92503e350623d550138bc0f593ba2626788260
Justin Ball <justinball@gmail.com>
2015-05-13 10:15:38 -0600

    Fixed spelling error.

--- client/js/components/admin/dashboard.spec.js
+++ client/js/components/admin/dashboard.spec.js
@@ -3,7 +3,7 @@ import TestUtils          from 'react/lib/ReactTestUtils';
 import DashBoard          from './dashboard';
 
 describe('dashboard', function() {
-  it('renders the dashboar', function() {
+  it('renders the dashboard', function() {
     var result = TestUtils.renderIntoDocument(<DashBoard/>);
     expect(result.getDOMNode().textContent).toContain("Client Info");
   });
