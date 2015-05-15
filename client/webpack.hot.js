3bb7091656fe11d974dd02a790af918f63c1daf7
Justin Ball <justinball@gmail.com>
2015-05-15 12:58:50 -0600

    Don't compile packages in dev mode.

--- client/webpack.hot.js
+++ client/webpack.hot.js
@@ -4,33 +4,22 @@ var release           = false;
 var settings          = require('./config/settings.js');
 var webpackConfig     = require('./config/webpack.config.js')(release);
 
-var compiler = webpack(webpackConfig);
-
-console.log("Building bundles ......");
-
-compiler.run(function(err, stats){
-
+new webpackDevServer(webpack(webpackConfig), {
+  publicPath: webpackConfig.output.publicPath,
+  contentBase: settings.devOutput,
+  hot: true,
+  watch: true,
+  noInfo: true,
+  stats: { colors: true },
+  inline: true,
+  progress: true,
+  headers: { "Access-Control-Allow-Origin": "*" },
+  filename: '[name]_web_pack_bundle.js',
+  noInfo: false,
+  quiet: false,
+}).listen(settings.ports.hotPort, 'localhost', function(err, result){
   if(err){
-    console.log(err);
-  } else {
-    new webpackDevServer(webpack(webpackConfig), {
-      publicPath: webpackConfig.output.publicPath,
-      hot: true,
-      watch: true,
-      noInfo: true,
-      colors: true,
-      inline: true,
-      progress: true,
-      headers: { "Access-Control-Allow-Origin": "*" },
-      filename: '[name]_web_pack_bundle.js',
-      noInfo: false,
-      quiet: true,
-    }).listen(settings.ports.hotPort, 'localhost', function(err, result){
-      if(err){
-        console.log('webpack-dev-server', err);
-      }
-      console.log('Webpack hot load server listening on: ' + webpackConfig.output.publicPath);
-     });
+    console.log('webpack-dev-server', err);
   }
-
-});
+  console.log('Webpack hot load server listening on: ' + webpackConfig.output.publicPath);
+ });
