aece5460d68edc25ee0339d835ed69f5ce23634d
Justin Ball <justinball@gmail.com>
2015-05-15 17:02:23 -0600

    Removed unused values from release config.

--- client/webpack.release.js
+++ client/webpack.release.js
@@ -1,6 +1,4 @@
-var webpack           = require('webpack');
 var release           = true;
-var settings          = require('./config/settings.js');
 var webpackConfig     = require('./config/webpack.config.js')(release);
 
 module.exports = webpackConfig;
