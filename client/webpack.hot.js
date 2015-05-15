358f36bcdefef9b8f79f4eadcddd3a92b69819b4
Justin Ball <justinball@gmail.com>
2015-05-15 17:02:11 -0600

    Refactored hot loader config.

--- client/webpack.hot.js
+++ client/webpack.hot.js
@@ -6,20 +6,15 @@ var webpackConfig     = require('./config/webpack.config.js')(release);
 
 new webpackDevServer(webpack(webpackConfig), {
   publicPath: webpackConfig.output.publicPath,
-  contentBase: settings.devOutput,
-  hot: true,
   watch: true,
-  noInfo: true,
-  stats: { colors: true },
+  hot: true,
   inline: true,
   progress: true,
   headers: { "Access-Control-Allow-Origin": "*" },
   filename: '[name]_web_pack_bundle.js',
-  noInfo: false,
-  quiet: false,
 }).listen(settings.ports.hotPort, 'localhost', function(err, result){
   if(err){
     console.log('webpack-dev-server', err);
   }
   console.log('Webpack hot load server listening on: ' + webpackConfig.output.publicPath);
- });
+});
