f673f16bd5dbd39484d22a65344ae7238a84198c
Justin Ball <justinball@gmail.com>
2015-05-14 22:43:25 -0600

    Removed settings

--- client/js/routes.spec.js
+++ client/js/routes.spec.js
@@ -1,20 +1,18 @@
 import React    from 'react';
 import Router   from 'react-router';
 
-import Settings from './utils/settings';
-import routes   from './routes';
+import Routes   from './routes';
 
 var Route = Router.Route;
 
 describe('default route', function () {
 
   it('renders assessment', function (done) {
-    Router.run(routes, '/', function (Handler, state) {
+    Router.run(Routes, '/', function (Handler, state) {
     	var globalSettings = {
     		srcUrl: "http://www.example.com"
     	}
-    	var settings = Settings.load(globalSettings);
-      var html = React.renderToString(<Handler params={state.params} settings={settings} />);
+    	var html = React.renderToString(<Handler params={state.params} />);
       expect(html).toMatch(/assessment/);
       done();
     });
