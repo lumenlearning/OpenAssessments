49118202503b685fdddf413c86e9006b086ff651
James Carbine <jamescarbine@gmail.com>
2015-05-18 22:54:39 -0600

    Add shared partial for default settings javascript rendering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,14 @@ module ApplicationHelper
   def canvas_url
     session[:canvas_url] || Rails.application.secrets.canvas_url
   end
-  
+
   def application_base_url
     request.original_url.gsub(request.original_fullpath, '')
+  end
+
+  def jwt_token
+    return unless signed_in?
+    AuthToken.issue_token({ user_id: current_user.id })
   end
 
 end
