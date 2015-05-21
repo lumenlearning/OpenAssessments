e2dfe3fe29bdb273f2ba9fb4d4740642300c4e92
bensmith <benhsmith13@gmail.com>
2015-05-21 11:11:24 -0600

    Add handle login and validate email spec

--- client/js/components/admin/login.spec.js
+++ client/js/components/admin/login.spec.js
@@ -7,13 +7,11 @@ describe ('Admin login', function(){
   var login;
   var textFields;
   var form;
-  var button;
 
   beforeEach(function(){
     login = TestUtils.renderIntoDocument(<Login />);
     textFields = TestUtils.scryRenderedDOMComponentsWithClass(login, 'mui-text-field');
     form = TestUtils.findRenderedDOMComponentWithTag(login, 'form');
-    button = TestUtils.findRenderedDOMComponentWithClass(login, 'login-button');
   });
 
   it('Renders the login component', function(){
@@ -27,7 +25,6 @@ describe ('Admin login', function(){
   });
 
   it('outputs a validation error if no email is provided', function(){
-    var form = TestUtils.findRenderedDOMComponentWithTag(login, 'form');
     TestUtils.Simulate.submit(form);
 
     var email = Utils.findTextField(textFields, 'email');
@@ -35,23 +32,29 @@ describe ('Admin login', function(){
     expect(email.getDOMNode().textContent).toContain('Invalid email');
   });
 
-  describe('the form is submitted', function(){
+  xit('It submits the form when the Login button is pressed', function(){
+    var submitSpy = jasmine.createSpy();
+    var container = document.createElement('div');
+    var instance = React.render(<Login />, container);
+    var button = instance.findDOMNode('login-button');
+    container.addEventListener('onsubmit', submitSpy, false);
+    TestUtils.Simulate.click(button);
+    expect(submitSpy).toHaveBeenCalled();
+    container.removeEventListener('onsubmit', submitSpy, false);
+  });
 
-    beforeEach(function(){
-      spyOn(login, 'handleLogin');
+  it('It calls the handleLogin method when the form is submitted', function(){ //The submit has to be called twice, for reasons unknown
+    spyOn(login, 'handleLogin');
+    TestUtils.Simulate.submit(form);
+    TestUtils.Simulate.submit(form);
+    expect(login.handleLogin).toHaveBeenCalled();
+  });
 
-    });
-
-    it('It calls the handleLogin method', function(){ //The submit has to be called twice, for reasons unknown
-      TestUtils.Simulate.submit(form);
-      TestUtils.Simulate.submit(form);
-      expect(login.handleLogin).toHaveBeenCalled();
-    });
-
-    // it('It calls the handleLogin method when the submit button is clicked', function(){
-    //   TestUtils.Simulate.click(button);
-    //   expect(login.handleLogin).toHaveBeenCalled();
-    // });
+  it('It calls the validateEmail when the form is submitted', function(){
+    spyOn(login, 'validateEmail');
+    TestUtils.Simulate.submit(form);
+    expect(login.validateEmail).toHaveBeenCalled();
   });
 
 });
+
