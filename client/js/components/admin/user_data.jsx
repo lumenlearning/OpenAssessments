0cd589521595c3223a77cf8c722607ac174efaec
bensmith <benhsmith13@gmail.com>
2015-05-26 13:09:55 -0600

    Remove unneccisary comma

--- client/js/components/admin/user_data.jsx
+++ client/js/components/admin/user_data.jsx
@@ -84,7 +84,7 @@ export default React.createClass({
 
     var dropDownItems = [
       {payload: '0', text: 'End User'},
-      {payload: '1', text: 'Admin'},
+      {payload: '1', text: 'Admin'}
     ];
 
     return(
