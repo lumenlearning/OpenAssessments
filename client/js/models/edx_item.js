4803947098a54b218dc09b84bf12258d1a1d093e
dittonjs <jditton.atomic@gmail.com>
2015-06-01 12:00:22 -0600

    parsed edx numeric input

--- client/js/models/edx_item.js
+++ client/js/models/edx_item.js
@@ -15,8 +15,8 @@ export default class EdXItem{
         material : xml.find('choicegroup').attr('label'),
         answers  : [] 
       };
-      item.answers = EdXItem.parseAnswers(xml);
       item.question_type = EdX.questionType(xml);
+      item.answers = EdXItem.parseAnswers(xml, item.question_type);
 
       return item;
     } else {
@@ -24,15 +24,35 @@ export default class EdXItem{
     }
   }
 
-  static parseAnswers(xml){
-    var answers = xml.find('choice').map((index, item) => {
-      return {
-        id       : index,
-        material : item.textContent,
-        xml      : item,
-        correct  : item.getAttribute("correct")
-      };
-    });
+  static parseAnswers(xml, questionType){
+    var answers;
+    if(questionType == "edx_numerical_input"){
+      // TODO implement numerical input answers
+      
+      answers = xml.find('numericalresponse').map((index, item) => {
+        return {
+          id       : index,
+          material : item.getElementsByTagName("formulaequationinput")[0].getAttribute("label"),
+          xml      : item,
+          correct  : item.getAttribute("answer") 
+        };
+      });
+    } else if (questionType == "edx_multiple_choice"){
+      answers = xml.find('choice').map((index, item) => {
+        return {
+          id       : index,
+          material : item.textContent,
+          xml      : item,
+          correct  : item.getAttribute("correct")
+        };
+      });
+    } else if(questionType == "edx_dropdown"){
+
+    } else if(questionType == "edx_text_input"){
+      
+    } else if(questionType == "edx_drag_and_drop"){
+      
+    }
     return answers;
   }
 
