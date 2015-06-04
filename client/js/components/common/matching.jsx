93bbb2bf1f4a659922d68a48272562b6779030ca
dittonjs <jditton.atomic@gmail.com>
2015-06-04 08:06:07 -0600

    setup matching answer

--- client/js/components/common/matching.jsx
+++ client/js/components/common/matching.jsx
@@ -6,7 +6,7 @@ import AssessmentActions  from "../../actions/assessment";
 export default class Matching extends React.Component{
 
   answerSelected(){
-    AssessmentActions.answerSelected(this.props.item.id);
+    AssessmentActions.answerSelected(this.props.item);
   }
 
 
@@ -20,7 +20,7 @@ export default class Matching extends React.Component{
     return(
       <div>
         {this.props.item.matchMaterial}
-        <select>
+        <select ref="match" onChange={()=>{ this.answerSelected()}}>
           {materialItems}
         </select>
       </div>
