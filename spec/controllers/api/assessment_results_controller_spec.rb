d529fa9c349e14be92eb2c30c14b6ef9d0203d2d
Justin Ball <justinball@gmail.com>
2015-06-09 19:00:29 -0600

    Added spec

---
+++ spec/controllers/api/assessment_results_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe Api::AssessmentResultsController do
+
+  describe "POST create" do
+    it "creates an assessment result" do
+      assessment_result = FactoryGirl.build(:assessment_result)
+      post :create, id: assessment_result.id, eid: "foo", format: :json
+      expect(AssessmentResult.first).to_not be(nil)
+      expect(AssessmentResult.first.eid).to eq("foo")
+    end
+    it "creates an assessment result with an src url" do
+      assessment_result = FactoryGirl.build(:assessment_result)
+      post :create, id: assessment_result.id, src_url: "foo", format: :json
+      expect(AssessmentResult.first).to_not be(nil)
+      expect(AssessmentResult.first.src_url).to eq("foo")
+    end
+    it "creates an assessment result with an src url" do
+      assessment_result = FactoryGirl.build(:assessment_result)
+      post :create, id: assessment_result.id, identifier: "foo", format: :json
+      expect(AssessmentResult.first).to_not be(nil)
+      expect(AssessmentResult.first.identifier).to eq("foo")
+    end
+    it "creates an assessment result with keywords" do
+      assessment_result = FactoryGirl.build(:assessment_result)
+      post :create, id: assessment_result.id, keywords: "foo, bar", format: :json
+      expect(AssessmentResult.first).to_not be(nil)
+      expect(AssessmentResult.first.keyword_list).to include("foo")
+      expect(AssessmentResult.first.keyword_list).to include("bar")
+    end
+  end
+
+end
