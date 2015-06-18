dc4d9992ccc53fe4e2a33d635017f32060d535f4
Justin Ball <justinball@gmail.com>
2015-06-18 10:38:05 -0600

    Fixed ngrok

--- ngrok.rb
+++ ngrok.rb
@@ -13,10 +13,11 @@ def wait
       puts "Waiting for ngrok to start"
     end  
   end
+  sleep 0.5
 end
 
 def tunnel(config)
-  exec %Q{curl -X POST -H "Accept: application/json" -H "Content-type: application/json" -d '#{config}' http://localhost:4040/api/tunnels}
+  %Q{curl -X POST -H "Accept: application/json" -H "Content-type: application/json" -d '#{config}' http://localhost:4040/api/tunnels}
 end
 
 def config(subdomain, port)
@@ -24,6 +25,13 @@ def config(subdomain, port)
 end
 
 wait
+
 puts "Starting tunnels"
-tunnel(config(ENV['APP_SUBDOMAIN'], ENV['APP_PORT']))
-tunnel(config(ENV['ASSETS_SUBDOMAIN'], ENV['ASSETS_PORT']))
+app = tunnel(config(ENV['APP_SUBDOMAIN'], ENV['APP_PORT']))
+assets = tunnel(config(ENV['ASSETS_SUBDOMAIN'], ENV['ASSETS_PORT']))
+
+system app
+sleep 0.5  # Sleep is ugly, but if you make request to ngrok too quickly it will panic and die.
+system assets
+
+sleep # Don't exit. Sleep forever so that Foreman doesn't die
