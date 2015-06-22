12294ce25e56538df6a11e5dbe3d05b0e631fe81
bensmith <benhsmith13@gmail.com>
2015-06-22 10:53:24 -0600

    Adds name proptype validation

--- client/js/components/common/option.jsx
+++ client/js/components/common/option.jsx
@@ -5,24 +5,24 @@ import AssessmentActions  from "../../actions/assessment";
 
 export default class Option extends React.Component{
 
-	answerSelected(){
-		//AssessmentActions.answerSelected(this.props.item.id);
-	}
+  answerSelected(){
+    //AssessmentActions.answerSelected(this.props.item.id);
+  }
+  render(){
 
-	render(){
-
-		var materialItems = this.props.item.material.map((mat) =>{
-					return <option value={mat} name={this.props.name}>{mat}</option>;
-				});
-			return(
-			<div>
-				<select onChange={()=>{this.answerSelected()}}>
-					{materialItems}
-				</select>
-			</div>
-		);
-	}
+    var materialItems = this.props.item.material.map((mat) =>{
+          return <option value={mat} name={this.props.name}>{mat}</option>;
+    });
+    return(
+      <div>
+		<select onChange={()=>{this.answerSelected()}}>
+          {materialItems}
+        </select>
+      </div>
+    );
+  }
 }
 Option.propTypes = {
-	item: React.PropTypes.object.isRequired
+  item: React.PropTypes.object.isRequired,
+  name: React.PropTypes.string.isRequired
 };
