abaf5a70d9770869fb95f9fd17473bfa810035ab
Justin Ball <justinball@gmail.com>
2015-06-23 10:15:21 -0600

    Replaced ngrok with localtunnel.m

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ else
   accounts = [{
     code: 'canvasstarterapp',
     name: 'Canvas Starter App',
-    domain: 'http://canvasstarterapp.ngrok.com',
+    domain: 'http://canvasstarterapp.localtunnel.me',
     lti_key: 'canvasstarterapp',
     lti_secret: 'd52ca2c9892975bbb9def56e68eefe8e92a338d9b74d73ec5dad64803a376b2f1f5129c0bd9f7e73684526c234e0835bd1635e09d427cd45cb0de4296278682f',
     canvas_uri: 'https://canvas.instructure.com'
