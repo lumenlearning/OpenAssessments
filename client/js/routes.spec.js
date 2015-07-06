c4b9efef221e60e28da5bd66e375c1c38eb147b8
Benjamin Call <colebusby@Coles-MacBook-Pro.local>
2015-07-06 11:10:48 -0600

    completed specs

--- client/js/routes.spec.js
+++ client/js/routes.spec.js
@@ -1,16 +1,36 @@
-import React    from 'react';
-import Router   from 'react-router';
+import React              from 'react';
+import Router             from 'react-router';
+import routes             from './routes';
+// import SettingsActions    from '../../actions/settings';
+// import UserActions        from '../actions/user';
 
-import routes   from './routes';
+// var Route = Router.Route;
 
-var Route = Router.Route;
+// describe('Logged in', function () {
 
-describe('default route', function () {
-  it('renders home', function (done) {
-    Router.run(routes, '/', function (Handler, state){
-      var html = React.renderToString(<Handler params={state.params} />);
-      expect(html).toMatch(/Home/);
-      done();
+//  helpStubAjax(SettingsActions);
+
+//   UserActions.login({
+//     data: {
+//       body: {
+//         email: "joe@example.com",
+//         displayName: "joe",
+//         jwt_token: "asdf"
+//       }
+//     }
+//   });
+//   jasmine.clock().tick(); // Advance the clock to the next tick
+  localStorage.setItem('jwt', "asdfsd");
+
+
+  describe('default route', function () {
+    it('renders home', function (done) {
+      Router.run(routes, '/', function (Handler, state){
+        var html = React.renderToString(<Handler params={state.params} />);
+        expect(html).toMatch(/Home/);
+        done();
+      });
     });
   });
+
 });
