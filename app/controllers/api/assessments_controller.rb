4fdbd43c0892cccc620e89659fa7b32946df1c0e
dittonjs <jditton.atomic@gmail.com>
2015-07-10 16:11:01 -0600

    added notes about different url params

--- app/controllers/api/assessments_controller.rb
+++ app/controllers/api/assessments_controller.rb
@@ -27,7 +27,27 @@ class Api::AssessmentsController < ApplicationController
     end
   end
 
+  # *******************************************************************
+  # URL PARAMS
+  # 
+  # enable_start
+  # => set to true or false to show the start screen 
+  # style
+  # => set to "lumen_learning" to use the lumen learning theme. Leave out for default style
+  # asid
+  # => give it an id to load an assessment setting which determines how many attempts a studen has to take the quiz. Right now there is no
+  #    way to create one of these so if you need one you can find the assessment you want in the database and create one by doing .assessment_settings.create({attempts: n})
+  # per_sec
+  # => give it the number of random questions from each section you want.
+  # confidence_levels
+  # => set to true or false to display confidence controls
+  # Example
+  # https://assessments.lumenlearning.com/assessments/15?style=lumen_learning&asid=1&per_sec=2&confidence_levels=true&enable_start=true
+  # ********************************************************************
   def create
+
+ 
+
     assessment = Assessment.from_xml(request.body.read, current_user)
     respond_with(:api, assessment)
   end
