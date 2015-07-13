1d2381e5a9f19f6e5d708d48510d5dc2b4aebc5c
Justin Ball <justinball@gmail.com>
2015-07-13 15:40:00 -0600

    Updated seeds to use env vars

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,23 +2,22 @@ admin = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << admin.email
 
 # Setup default accounts
+canvas_uri = ENV["APP_DEFAULT_CANVAS_URL"] || 'https://canvas.instructure.com'
+
 if Rails.env.production?
-  accounts = [{
-    code: ENV["APP_SUBDOMAIN"],
-    name: Rails.application.secrets.application_name,
-    domain: Rails.application.secrets.application_url,
-    lti_key: ENV["APP_SUBDOMAIN"],
-    canvas_uri: 'https://canvas.instructure.com'
-  }]
+  canvas_uri ||= 'https://canvas.instructure.com'
 else
-  accounts = [{
-    code: ENV["APP_SUBDOMAIN"],
-    name: Rails.application.secrets.application_name,
-    domain: Rails.application.secrets.application_url,
-    lti_key: ENV["APP_SUBDOMAIN"],
-    canvas_uri: 'https://atomicjolt.instructure.com'
-  }]
+  canvas_uri ||= 'https://atomicjolt.instructure.com'
 end
+
+accounts = [{
+  code: ENV["APP_SUBDOMAIN"],
+  name: Rails.application.secrets.application_name,
+  domain: Rails.application.secrets.application_url,
+  lti_key: ENV["APP_SUBDOMAIN"],
+  canvas_uri: canvas_uri
+}]
+
 
 # Setup accounts
 accounts.each do |account|
@@ -43,3 +42,4 @@ if assessment = Assessment.find_by(title: 'drupal.xml')
   assessment.recommended_height = 960
   assessment.save!
 end
+
