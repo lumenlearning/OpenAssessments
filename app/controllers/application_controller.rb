2186bdd5935747b5d90fca077f1b1e118a1a17d7
Justin Ball <justinball@gmail.com>
2015-07-17 18:28:05 -0600

    Removed protect account for now.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :protect_account
+  #before_action :protect_account
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   helper_method :current_account
