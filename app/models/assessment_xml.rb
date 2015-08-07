71667ebc202c2b3b129ae9b19660b02afbc362f5
Bracken Mosbacker <bracken@lumenlearning.com>
2015-08-07 08:42:06 -0700

    limit questions per section server-side
    if this is done only client-side then a user
    could see all the questions anyway.
    
    Test Plan:
     * xml should be returned as normal if `per_sec` not set
     * when `per_sec` is set, the xml returned should only have that many items in each section
     * the front-end code trying to select items from the section should still work if there are more items in a section than `per_sec`

--- app/models/assessment_xml.rb
+++ app/models/assessment_xml.rb
@@ -7,4 +7,16 @@ class AssessmentXml < ActiveRecord::Base
     where(kind: kind)
   end
 
+  def xml_with_limited_questions(per_section)
+    node = Nokogiri::XML(self.xml)
+
+    node.css('section section').each do |s|
+      while s.css('item').count > per_section
+        s.css('item')[rand(s.css('item').count)].remove
+      end
+    end
+
+    node.to_xml
+  end
+
 end
