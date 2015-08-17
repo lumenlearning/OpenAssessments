f1b3e97f8e5f21de3957b8c4d8b97e9be140ac45
dittonjs <jditton.atomic@gmail.com>
2015-08-17 12:14:43 -0600

    fixed failing specs

--- client/js/components/admin/account_dashboard.spec.js
+++ client/js/components/admin/account_dashboard.spec.js
@@ -19,28 +19,27 @@ describe('account_dashboard', function() {
 
   beforeEach(()=>{
     AccountsActions.loadAccounts();
-    jasmine.Ajax.requests.mostRecent().respondWith({
-      "status"        : 200,
-      "contentType"     : "text/plain",
-      "responseText" : JSON.stringify([{canvas_uri: "https://canvas.instructure.com",
-        code: "canvasstarterapp",
-        created_at: "2015-05-08T19:06:02.755Z",
-        domain: "canvasstarterapp.ngrok.io",
-        id: 1, lti_key: "canvasstarterapp",
-        lti_secret: "d52ca2c9892975bbb9def56e68eefe8e92a338d9b74d73ec5dad64803a376b2f1f5129c0bd9f7e73684526c234e0835bd1635e09d427cd45cb0de4296278682f",
-        name: "Canvas Stater App",
-        updated_at: "2015-05-08T19:06:02.755Z"}])
-    });
+    // jasmine.Ajax.requests.mostRecent().respondWith({
+    //   "status"        : 200,
+    //   "contentType"     : "text/plain",
+    //   "responseText" : JSON.stringify([{canvas_uri: "https://canvas.instructure.com",
+    //     code: "canvasstarterapp",
+    //     created_at: "2015-05-08T19:06:02.755Z",
+    //     domain: "canvasstarterapp.ngrok.io",
+    //     id: 1, lti_key: "canvasstarterapp",
+    //     lti_secret: "d52ca2c9892975bbb9def56e68eefe8e92a338d9b74d73ec5dad64803a376b2f1f5129c0bd9f7e73684526c234e0835bd1635e09d427cd45cb0de4296278682f",
+    //     name: "Canvas Stater App",
+    //     updated_at: "2015-05-08T19:06:02.755Z"}])
+    // });
 
     jasmine.clock().tick();
     dashboard = TestUtils.renderIntoDocument(<Subject />);
-    AccountsActions.loadAccounts();
+    //AccountsActions.loadAccounts();
      // Advance the clock to the next tick
     currentAccount = AccountsStore.accountById(accountId);
     currentAccountName = currentAccount.name;
   });
-  afterEach(()=>{
-  });
+  
   it('renders the account dashboard', function() {
     expect(dashboard).toBeDefined();
   });
@@ -50,8 +49,5 @@ describe('account_dashboard', function() {
     expect(React.findDOMNode(result).textContent).toContain(currentAccountName);
   });
 
-  afterEach(()=>{
-    React.unmountComponentAtNode(React.findDOMNode(dashboard).parentNode);
-  });
 
 });
