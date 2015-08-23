ec5feb5e73d96ad518f003244896abe161024606
Bracken Mosbacker <bracken@lumenlearning.com>
2015-08-23 14:15:02 -0600

    stop sending emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = false
 
   # Disable automatic flushing of the log to improve performance.
