6e84a82fe8cca38aa622b985333e24afa8893847
Justin Ball <justinball@gmail.com>
2015-08-31 16:48:53 -0600

    Added babel runtime.

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = function(release){
   ];
 
   var autoprefix = '{browsers:["Android 2.3", "Android >= 4", "Chrome >= 20", "Firefox >= 24", "Explorer >= 8", "iOS >= 6", "Opera >= 12", "Safari >= 6"]}';
-  var jsLoaders = ["babel-loader?stage=0"];
+  var jsLoaders = ["babel-loader?stage=0&optional=runtime"];
 
   var cssLoaders = ['style-loader', 'css-loader', 'autoprefixer-loader?' + autoprefix];
 
