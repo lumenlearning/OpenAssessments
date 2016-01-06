3551668968ebead5d7720cb60ecb1bf5d443a728
JuliannaGreen <juliannaggreen@gmail.com>
2016-01-06 12:29:20 -0800

    Make student selections show in quiz-attempt summary
    It can be helpful for a teacher to be reminded of the
    correct answers and to see which incorrect answer the
    student chose.
    The incorrect answer may be an indicator of a concept
    that the student needs additional help with.
    
    Test Plan:
    
    -Go to course with teacher role and click "Quiz 1"
    -Click "Manage Quiz Attempts"
    -Click on a quiz-attempt
    -Notice the green checks showing the correct answers and
    the red x's marking incorrect answers the student chose.

--- client/js/components/common/checkbox.jsx
+++ client/js/components/common/checkbox.jsx
@@ -3,70 +3,18 @@
 import React              from 'react';
 import AssessmentActions  from "../../actions/assessment";
 import AssessmentStore    from "../../stores/assessment";
+import Styles             from "../../themes/selection.js";
+
+const styles = Styles;
 
 export default class CheckBox extends React.Component{
+
 
   answerSelected(){
     AssessmentActions.answerSelected(this.props.item);
   }
 
-  getStyles(props, theme){
-    return {
-      btnQuestion:{
-        whiteSpace: theme.btnQuestionWhiteSpace,
-        background: "transparent",//props.isDisabled? "transparent" : theme.btnQuestionBackground,
-        color: theme.btnQuestionColor,
-        textAlign: theme.btnQuestionTextAlign,
-        padding: theme.btnQuestionPadding,
-        marginBottom: theme.btnQuestionMarginBottom,
-        display: theme.btnQuestionDisplay,
-        width: theme.btnQuestionWidth,
-        verticalAlign: theme.btnQuestionVerticalAlign,
-        fontWeight: theme.btnQuestionFontWeight,
-        touchAction: theme.btnQuestionTouchAction,
-        cursor: theme.btnQuestionCursor,
-        border: theme.btnQuestionBorder,
-        fontSize: theme.btnQuestionFontSize,
-        lineHeight: theme.btnQuestionLineHeight,
-        borderRadius: theme.btnQuestionBorderRadius
-      },
-      span: {
-        fontWeight: "normal !important",
-        marginLeft: "5px"
-      },
-      checkStyleCorrect: {
-        float: "left",
-        color: "white",
-        fontSize: ".9em",
-        backgroundColor: "#4EAA59",
-        width: "26px",
-        height: "22px",
-        paddingTop: "2px",
-        paddingLeft: "8px",
-        marginTop: "2px",
-        marginLeft: "-30px",
-        borderRadius: "4px",
-        cursor: "default"
-      },
-      checkStyleWrong: {
-        float: "left",
-        color: "white",
-        fontSize: ".8em",
-        backgroundColor: "#C3092B",
-        width: "26px",
-        height: "22px",
-        paddingTop: "3px",
-        paddingLeft: "9px",
-        marginTop: "2px",
-        marginLeft: "-30px",
-        borderRadius: "4px",
-        cursor: "default"
-      }
-    }
-  }
-
-  render(){
-    var styles = this.getStyles(this.props, this.context.theme);
+  checkedStatus(){
     var checked = null;
     var optionFlag = null;
     if( this.props.checked === true ) {
@@ -76,24 +24,32 @@ export default class CheckBox extends React.Component{
     } else if ( !this.props.isDisabled ) {
       checked = (AssessmentStore.studentAnswers() && AssessmentStore.studentAnswers().indexOf(this.props.item.id) > -1) ? "true" : null;
     }
-    var checkBox = <input type="checkbox" defaultChecked={checked} disabled={this.props.isDisabled} name={this.props.name} onClick={()=>{ this.answerSelected() }}/>;
+    return checked;
+  }
 
+  optionFlagStatus(){
     if(this.props.showAsCorrect){
       var label = "Correct Answer that was ";
-      label += checked ? "chosen" : "not chosen";
+      var optionFlag;
+      label += this.checkedStatus() ? "chosen" : "not chosen";
       optionFlag = <div className="correctIndicator" aria-label={label} style={styles.checkStyleCorrect}>&#10003;</div>;
-    } else if (this.props.showAsCorrect === false && checked){
+    } else if (this.props.showAsCorrect === false && this.checkedStatus()){
       optionFlag = <div className="wrongIndicator" aria-label="Wrong answer that was chosen" style={styles.checkStyleWrong}>&#10008;</div>;
     }
+    return optionFlag;
+  }
 
-    return (<div>
-      {optionFlag}
-      <div className="btn btn-block btn-question" style={styles.btnQuestion}>
-        <label>
-          {checkBox}
-          <span style={styles.span}>{this.props.item.material}</span>
-        </label>
-      </div>
+  render(){
+
+    return (
+      <div>
+        {this.optionFlagStatus()}
+        <div className="btn btn-block btn-question" style={styles.btnQuestion}>
+          <label>
+            <input type="checkbox" defaultChecked={this.checkedStatus()} disabled={this.props.isDisabled} name={this.props.name} onClick={()=>{ this.answerSelected() }}/>
+            <span style={styles.span}>{this.props.item.material}</span>
+          </label>
+        </div>
       </div>
     );
   }
--- client/js/components/common/radio_button.jsx
+++ client/js/components/common/radio_button.jsx
@@ -3,58 +3,79 @@
 import React              from 'react';
 import AssessmentActions  from "../../actions/assessment";
 import AssessmentStore    from "../../stores/assessment";
+import Styles             from "../../themes/selection.js";
+
+const styles = Styles;
 
 export default class RadioButton extends React.Component{
-  
+
   answerSelected(){
     AssessmentActions.answerSelected(this.props.item);
   }
 
-  getStyles(props, theme){
-    return {
-      btnQuestion:{
-        whiteSpace: theme.btnQuestionWhiteSpace,
-        background: "transparent",//props.isDisabled? "transparent" : theme.btnQuestionBackground,
-        color: theme.btnQuestionColor,
-        textAlign: theme.btnQuestionTextAlign,
-        padding: theme.btnQuestionPadding,
-        marginBottom: theme.btnQuestionMarginBottom,
-        display: theme.btnQuestionDisplay,
-        width: theme.btnQuestionWidth,
-        verticalAlign: theme.btnQuestionVerticalAlign,
-        fontWeight: theme.btnQuestionFontWeight,
-        touchAction: theme.btnQuestionTouchAction,
-        cursor: theme.btnQuestionCursor,
-        border: theme.btnQuestionBorder,
-        fontSize: theme.btnQuestionFontSize,
-        lineHeight: theme.btnQuestionLineHeight,
-        borderRadius: theme.btnQuestionBorderRadius
-      },
-
-      radioText: {
-        color: theme.radioTextColor,
-        fontWeight: theme.radioTextFontWeight,
-        marginLeft: theme.radioTextMarginLeft,
-      }
+  checkedStatus(){
+    var checked = null;
+    var optionFlag = null;
+    if( this.props.checked === true ) {
+      checked = "true";
+    } else if ( this.props.checked === false ){
+      checked = false;
+    } else if ( !this.props.isDisabled ) {
+      checked = (AssessmentStore.studentAnswers() && AssessmentStore.studentAnswers().indexOf(this.props.item.id) > -1) ? "true" : null;
     }
+    return checked;
   }
+
+  optionFlagStatus(){
+    if(this.props.showAsCorrect){
+      var label = "Correct Answer that was ";
+      var optionFlag;
+      label += this.checkedStatus() ? "chosen" : "not chosen";
+      optionFlag = <div className="correctIndicator" aria-label={label} style={styles.checkStyleCorrect}>&#10003;</div>;
+    } else if (this.props.showAsCorrect === false && this.checkedStatus()){
+      optionFlag = <div className="wrongIndicator" aria-label="Wrong answer that was chosen" style={styles.checkStyleWrong}>&#10008;</div>;
+    }
+    return optionFlag;
+  }
+
   render(){
-    var styles = this.getStyles(this.props, this.context.theme)
-    var checked = (this.props.item.id == AssessmentStore.studentAnswers()) ? "true" : null;
-    var radio = !this.props.isDisabled ? <input type="radio" defaultChecked={checked} name={this.props.name} onClick={()=>{ this.answerSelected() }}/> : <input type="radio" disabled="true" name={this.props.name} onClick={()=>{ this.answerSelected() }}/>;
+    var styles = Styles;
+    var checked = null;
+    var optionFlag = null;
+
+    if( this.props.checked === true ) {
+      checked = "true";
+    } else if ( this.props.checked === false ){
+      checked = false;
+    } else if ( !this.props.isDisabled ) {
+      checked = (AssessmentStore.studentAnswers() && AssessmentStore.studentAnswers().indexOf(this.props.item.id) > -1) ? "true" : null;
+    }
+
+    var radio = <input type="radio" defaultChecked={checked} disabled={this.props.isDisabled} name={this.props.name} onClick={()=>{ this.answerSelected() }}/>;
+
+    if(this.props.showAsCorrect){
+      var label = "Correct Answer that was ";
+      label += checked ? "chosen" : "not chosen";
+      optionFlag = <div className="correctIndicator" aria-label={label} style={styles.checkStyleCorrect}>&#10003;</div>;
+    } else if (this.props.showAsCorrect === false && checked){
+      optionFlag = <div className="wrongIndicator" aria-label="Wrong answer that was chosen" style={styles.checkStyleWrong}>&#10008;</div>;
+    }
+
     return (
-      
-      <div className="btn btn-block btn-question" style={styles.btnQuestion}>
-        <label>
-          {radio}
-          <span className="radio-text" style={styles.radioText}>{this.props.item.material}</span>
-        </label>
+      <div>
+        {this.optionFlagStatus()}
+        <div className="btn btn-block btn-question" style={styles.btnQuestion}>
+          <label>
+            <input type="radio" defaultChecked={this.checkedStatus()} disabled={this.props.isDisabled} name={this.props.name} onClick={()=>{ this.answerSelected() }}/>
+            <span style={styles.span}>{this.props.item.material}</span>
+          </label>
+        </div>
       </div>
     );
   }
 }
 
-RadioButton.propTypes = { 
+RadioButton.propTypes = {
   item: React.PropTypes.object.isRequired,
   name: React.PropTypes.string.isRequired,
   isDisabled: React.PropTypes.bool
