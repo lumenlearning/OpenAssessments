3a8f84fa174d96a97530579e92fd1f858acbd52d
dillon <dillonosman@gmail.com>
2017-05-04 16:26:56 -0700

    Multi drop downs now display properly in view mode for the edit quiz page.

--- client/js/components/edit/question_block/question_types/multi_dropdown.jsx
+++ client/js/components/edit/question_block/question_types/multi_dropdown.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import Style from '../css/style.js';
+import _ from 'lodash';
 //import any other dependencies here.
 
 export default class MultiDropdownFeedback extends Component {
@@ -16,23 +18,52 @@ export default class MultiDropdownFeedback extends Component {
 
   render() {
     let question = this.props.question;
+    let style = Style.styles();
+    let windowWidth = this.props.windowWidth
 
     //place JSX between the parens!
     return (
       <div>
         {Object.keys(question.dropdowns).map((key, i) => {
-          return question.dropdowns[key].map((dropdown, i) => {
-            let correctAnswer = question.correct.find((correctDropdown) => {
-              return dropdown.name === correctDropdown.name;
-            });
-
-            return (
-              <div>
-                <div>Answer: </div>
-                <div>Feedback: </div>
+          return (
+            <div style={style.qbAnswerTable}>
+              <h3>{key}</h3>
+              <div style={{display: "table", width: "100%"}}>
+                <div style={style.emptyCell}></div>
+                <div style={_.merge(style.labelBlock, style.label)}>Answers</div>
+                <div style={_.merge(style.labelBlock, style.label)}>Feedback</div>
               </div>
-            )
-          })
+              <div style={style.qbTblContent} >
+                {question.dropdowns[key].map((dropdown, i) => {
+                  let img = null;
+                  let answerFeedbackBlock = windowWidth <= 1000 ? {display: "table", width: "100%", borderSpacing: "0 10px"} : {display: "table", width: "100%", borderSpacing: "10px 8px"};
+                  let answerFeedback      = windowWidth <= 1000 ? {display: "block", width: "100%", marginBottom: "20px"} : {display: "table-cell", width: "50%"};
+                  let feedback = question.feedback[key+dropdown.value];
+                  let correctIndex = question.correct.findIndex((correctDropdown) => {
+                    return dropdown.value === correctDropdown.value && key === correctDropdown.name;
+                  });
+
+                  if(correctIndex >= 0){
+                    img = (<img style={style.checkOrExit} src="/assets/checkbox-48.png" alt="This Answer is Correct"/>);
+                  }
+
+                  return (
+                    <div style={{display: "table", width: "100%"}} >
+                      <div style={{display: "table-cell", minWidth: "50px", height: "100%", verticalAlign: "middle"}} >
+                        {img}
+                      </div>
+                      <div style={{display: "table-cell", width: "100%", verticalAlign: "top"}}>
+                        <div style={answerFeedbackBlock}>
+                          <div style={_.merge(answerFeedback, style.qbTblCell)} dangerouslySetInnerHTML={{__html:dropdown.name}} />
+                          <div style={_.merge(answerFeedback, style.qbTblCell)} dangerouslySetInnerHTML={{__html:feedback}} />
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )
         })}
       </div>
     );
