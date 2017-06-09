5bb8b18ef4cd11173f85ab94da62ac11dd3b7bfc
dillon <dillonosman@gmail.com>
2017-06-09 14:57:46 -0700

    Essay question can no longer be selected unless an essay question already exists.

--- client/js/components/edit/question_interface/question_type_selector.jsx
+++ client/js/components/edit/question_interface/question_type_selector.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Constants from '../../../constants.js';
+import AssessmentStore from '../../../stores/assessment.js';
 //import any other dependencies here.
 
 export default class QuestionTypeSelector extends Component {
@@ -16,8 +17,12 @@ export default class QuestionTypeSelector extends Component {
   }
 
   render() {
+    let AllQuestions = AssessmentStore.allQuestions();
     let question = this.props.question;
     let questionTypes = Constants.QUESTION_TYPES;
+    let hasEssays = AllQuestions.findIndex((question) => {
+      return question.question_type == 'essay_question'
+    });
 
     return (
       <div>
@@ -25,7 +30,12 @@ export default class QuestionTypeSelector extends Component {
         <select value={!!question.question_type ? question.question_type : null} onChange={this.props.handleQuestionTypeChange} style={{marginBottom: '25px'}}>
           {!!question.question_type ? null : <option value={null} disabled={true} selected={true}>Select Question Type</option>}
           {questionTypes.map((type) => {
-            return <option value={type.value} >{type.name}</option>
+            if(hasEssays > -1){
+              return <option value={type.value} >{type.name}</option>
+            }
+            else if(hasEssays === -1 && type.value !== 'essay_question'){
+              return <option value={type.value} >{type.name}</option>
+            }
           })}
         </select>
       </div>
