92cae13f4a080024ffaf2f68334cf9c66e0ebae4
Bracken Mosbacker <bracken@lumenlearning.com>
2018-05-25 09:13:31 -0700

    allow generating tokens that don't expire
    So that we can give a token to the other services that need
    to use this api

--- lib/auth_token.rb
+++ lib/auth_token.rb
@@ -9,7 +9,7 @@ module AuthToken
   # http://self-issued.info/docs/draft-ietf-oauth-json-web-token.html#rfc.section.4.1.6
   def AuthToken.issue_token(payload, exp=24.hours.from_now)
     payload['iat'] = DateTime.now.to_i    # issued at claim
-    payload['exp'] = exp.to_i             # Default expiration set to 24 hours.
+    payload['exp'] = exp.to_i if exp      # Default expiration set to 24 hours.
     payload['aud'] = Rails.application.secrets.auth0_client_id
     JWT.encode(payload, Rails.application.secrets.auth0_client_secret, 'HS512')
   end
