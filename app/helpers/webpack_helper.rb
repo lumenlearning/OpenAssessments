578ed3940a804c3b2c725dfc40e7a551ff1b0ae4
Bryan Borgeson <bryan@lumenlearning.com>
2018-06-06 17:03:59 -0700

    remove npm build command in favor of webpack rake task, knowing it will fail

--- app/helpers/webpack_helper.rb
+++ app/helpers/webpack_helper.rb
@@ -2,14 +2,14 @@ module WebpackHelper
 
   def webpack_bundle_tag(bundle)
     src =
-      # if Rails.configuration.webpack[:use_manifest]
-      #   manifest = Rails.configuration.webpack[:asset_manifest][bundle]
-      #   bundle = manifest.instance_of?(Array) ? manifest[0] : manifest
-      #
-      #   "/assets/#{bundle}"
-      # else
+      if Rails.configuration.webpack[:use_manifest]
+        manifest = Rails.configuration.webpack[:asset_manifest][bundle]
+        bundle = manifest.instance_of?(Array) ? manifest[0] : manifest
+
+        "#{}/assets/#{bundle}"
+      else
         "#{Rails.application.secrets.assets_url}/assets/#{bundle}_web_pack_bundle.js"
-      # end
+      end
 
     "<script src='#{src}' type='text/javascript'></script>".html_safe
   end
