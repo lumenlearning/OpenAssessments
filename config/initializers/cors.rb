dd95fa843910787c3a6ae3263b12e1bdac100960
Bryan Borgeson <bryan@lumenlearning.com>
2018-06-07 11:43:49 -0700

    tweak Rack::Cors middleware to rails 4 (instead of rails 5)

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,4 +1,4 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before 0, "Rack::Cors" do
   # Allows cross origin requests from anywhere for `/assets/` path to our Rails application.
   # We can lock this down further to just the CDN if we like. It'd be worth testing if there's
   # other implications.
