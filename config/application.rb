5fdfbd5a2958be4db1eb13554996568e3baaacf7
Bryan Borgeson <bryan@lumenlearning.com>
2018-06-07 12:34:36 -0700

    remove redundant cors code

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module CanvasStarterApp
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
 
