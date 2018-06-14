f4330e41a5758f91badbd904c58e2751e658080d
Bryan Borgeson <bryan@lumenlearning.com>
2018-06-14 13:06:11 -0700

    add font assets config per docs in font_assets gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = ENV['ASSETS_HOST']
+  config.font_assets.origin = ENV['FONT_ASSETS_ORIGIN']
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
