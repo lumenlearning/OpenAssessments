7f626013618442db00588e4c506e76cd701cb867
monkecheese <bryan@lumenlearning.com>
2019-05-07 11:48:50 -0700

    Make development health_check config pass
    The port number was incorrect for health checks against localhost
    and we don't actually care if mailer can't send in development env.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,18 +39,18 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.smtp_settings = {
-    :user_name => Rails.application.secrets.email_provider_username,
-    :password => Rails.application.secrets.email_provider_password,
-    :domain => Rails.application.secrets.domain_name,
-    :address => Rails.application.secrets.email_provider_address,
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  # config.action_mailer.smtp_settings = {
+  #   :user_name => Rails.application.secrets.email_provider_username,
+  #   :password => Rails.application.secrets.email_provider_password,
+  #   :domain => Rails.application.secrets.domain_name,
+  #   :address => Rails.application.secrets.email_provider_address,
+  #   :port => 587,
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  # }
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   # Send email in development mode?
