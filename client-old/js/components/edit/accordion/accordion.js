5d971c063efe6bddb4e98834a3cbfd29bb9fc1df
monkecheese <bryan@lumenlearning.com>
2019-05-30 22:14:59 -0700

    Migrate the remaining files from client-old
    I kept a backup version of client in the project for reference
    initially. This commit migrates the remaining files that existed
    before in client and removes the backup version of client.

--- client-old/js/components/edit/accordion/accordion.js
+++
@@ -1,6 +0,0 @@
-require('./styles/styles.css');
-
-import Accordion        from './accordionWrapper.jsx';
-import AccordionSection from './accordionSection.jsx';
-
-export default { Accordion, AccordionSection };
