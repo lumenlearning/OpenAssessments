b3f0b21fd61ef12dbcc821a714de683647a24741
Isaac Stefanek <isaac@lumenlearning.com>
2020-03-03 15:54:55 -0800

    WAYM-267 remove duplicate puma config

--- config/puma.rb
+++ config/puma.rb
@@ -12,13 +12,3 @@ on_worker_boot do
  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
  ActiveRecord::Base.establish_connection
 end
-
-on_worker_boot do
-  # Valid on Rails up to 4.1 the initializer method of setting `pool` size
-  ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-                Rails.application.config.database_configuration[Rails.env]
-    config['pool'] = ENV['MAX_THREADS'] || 5
-    ActiveRecord::Base.establish_connection(config)
-  end
-end
